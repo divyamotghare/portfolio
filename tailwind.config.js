edd5e52252172e9cbe03c6c0b653e9714f668a26
divyamotghare <divyamotghare9@gmail.com>
2024-07-14 16:44:56 +0530

    added color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      primary: "#ff4d6d",
+      secondary: "#a4133c",
+      white: "#ffffff",
+      pinkshade1: "#c9184a",
+      lightpink:'#ff8fa3'
+    },
     extend: {
-      backgroundImage: {
-        custom: "url('/radiant-gradient.png')",
+      // backgroundImage: {
+      //   custom: "url('/radiant-gradient.png')",
+      // },
+      backgroundColor: {
+        "primary-bg": "#fdf0d5",
+        "secondary-bg": "#780000",
+        white: "#ffffff",
       },
       fontFamily: {
         sans: ["Rajdhani", "sans-serif"],
